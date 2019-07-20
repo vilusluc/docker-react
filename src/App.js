4ed4ca3b9a5b2822a24a094945420bbc34f5ac25
Luckson Vilus <vilusl@gmail.com>
2019-07-19 21:35:29 -0400

    Changing the message

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          I was changed!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
